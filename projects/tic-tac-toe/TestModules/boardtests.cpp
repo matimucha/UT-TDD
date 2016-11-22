b59e5dd3df8ada0c117d2d3a43e758eae0f4f1bc
Mateusz Muszak <mateusz.muszak.ext@nokia.com>
2016-11-22 14:59:11 +0100

    ICE commit to repository after successfully made 2 additional tests

--- projects/tic-tac-toe/TestModules/boardtests.cpp
+++ projects/tic-tac-toe/TestModules/boardtests.cpp
@@ -9,19 +9,32 @@ class Board
     private:
         std::array<Field, 10> _fields;
     public:
-        Board() {}
+        Board()
+        {
+            std::fill(_fields.begin(), _fields.end(), Field::Blank);
+        }
+
         uint freeFieldsCount()
         {
-            return 9;
+            int _checkField = 0;
+            for(int i = 0; i < _fields.size(); ++i)
+            {
+                if(_fields.at(i) == Field::Blank)
+                    ++_checkField;
+            }
+            return (_checkField -1);
         }
+
         bool isWon()
         {
-            return (_fields.at(1) == _fields.at(2) && _fields.at(2) == _fields.at(3));
+            return (_fields.at(1) == _fields.at(2) && _fields.at(2) == _fields.at(3)
+                    && _fields.at(1) != Field::Blank && _fields.at(2) != Field::Blank
+                    && _fields.at(3) != Field::Blank);
         }
 
         bool isDraw()
         {
-            return false;
+            return (this->freeFieldsCount() == 0 && !(this->isWon()));
         }
 
         void setField(Position pos, Field field)
@@ -49,7 +62,7 @@ TEST(boardTests, newBoardAllFreeFields)
     ASSERT_THAT(board.freeFieldsCount(), testing::Eq(9) );
 }
 
-TEST(boardTests, isAnyPlayerWinner)
+TEST(boardTests, isPlayerXWinnerAtRow)
 {
     Board board;
 
@@ -59,4 +72,42 @@ TEST(boardTests, isAnyPlayerWinner)
     board.setField(Position(3), Field::X);
 
     ASSERT_THAT(board.isWon(), testing::Eq(true) );
+}
+
+TEST(boardTests, isPlayerOWinnerAtRow)
+{
+    Board board;
+
+    board.setField(Position(1), Field::O);
+    board.setField(Position(2), Field::O);
+
+    board.setField(Position(3), Field::O);
+
+    ASSERT_THAT(board.isWon(), testing::Eq(true) );
+}
+
+TEST(boardTests, isDrawAtEnd)
+{
+    Board board;
+    for(int i = 0; i < 9; ++i) //genration of random signs on board
+    {
+        if(i%2)
+            board.setField(Position(i+1), Field::X);
+        else
+            board.setField(Position(i+1), Field::O);
+    }
+    ASSERT_THAT(board.isDraw(), testing::Eq(true));
+}
+
+TEST(boardTests, isAllBoardFilled)
+{
+    Board board;
+    for(int i = 0; i < 9; ++i) //genration of random signs on board
+    {
+        if(i%2)
+            board.setField(Position(i+1), Field::X);
+        else
+            board.setField(Position(i+1), Field::O);
+    }
+    ASSERT_THAT(board.freeFieldsCount(), testing::Eq(0) );
 }
